4e8f6c0797acf4defde8f6e74561fdd680e62ef5
igleson <igleson.freire@vtex.com.br>
2025-02-06 18:31:55 -0300

    empty project

--- src/api/Storage/ResultTypes.cs
+++
@@ -1,3 +0,0 @@
-global using VolleyListingResult = api.Utils.Result<api.models.VolleyListing, api.Utils.Error>;
-global using VolleyListingSuccess = api.Utils.Success<api.models.VolleyListing, api.Utils.Error>;
-global using VolleyListingFailure = api.Utils.Failure<api.models.VolleyListing, api.Utils.Error>;
